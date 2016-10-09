4d6574d91f7186d22cf53ff46886c4430b407f44
Shang-Wei Wang <shang-wei@pave.com>
2016-10-09 01:50:45 -0400

    6.0

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 yes | sudo gem uninstall mosql
 gem build mosql.gemspec
-sudo gem install mosql-0.5.2.gem
+sudo gem install mosql-0.6.gem
 
 mosql -c pave_db.yml --sql 'postgres://localhost/template1' --mongo mongodb://localhost:27017 --only-db pave-dev --reimport --skip-tail
